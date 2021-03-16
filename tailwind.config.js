682778853540e02cf90ddf1009029a35a295f4df
TheBoringDude <iamcoderx@gmail.com>
2021-03-16 21:27:24 +0800

    post configurations

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      sans: ['"Source Sans Pro"', 'sans-serif']
+    },
     extend: {},
   },
   variants: {
